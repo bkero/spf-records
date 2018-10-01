69ce765d875b36bf54dcee5b136dc2735bdb31d6
Ben Kero <bkero@bke.ro>
2018-10-01 14:05:21 -0700

    handler.py: remove []s around call args, add shell

--- handler.py
+++ handler.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 """This file adds an event handler to execute sender_policy_flattener binary"""
-
 from subprocess import call, check_call
 
 
 def handler(event, context):
     """This is the handler that AWS Lambda should execute (handler.handler)"""
-    call(["./bin/spflat", "-c", "settings.json"])
+    call("./bin/spflat -c settings.json", shell=True)
     if check_call is 0:
         exit(0)
     else:
