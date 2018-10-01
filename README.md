dbd18fad1134a405ad5e24729a36e3b82a357d1f
Ben Kero <bkero@bke.ro>
2018-10-01 13:13:33 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This repository holds a configuration file and AWS Lambda handler for use with the sender_policy_flattener project located at https://github.com/cetanu/sender_policy_flattener
---
+++ handler.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""This file adds an event handler to execute sender_policy_flattener binary"""
+
+from subprocess import call, check_call
+
+
+def handler(event, context):
+    """This is the handler that AWS Lambda should execute (handler.handler)"""
+    call(["./bin/spflat", "-c", "settings.json"])
+    if check_call is 0:
+        exit(0)
+    else:
+        exit(1)
